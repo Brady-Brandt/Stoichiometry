a5bd94c03de382e270ff076db822f9c0a1d5d5a8
student <student@707HSSttAir2024.home>
2023-05-10 19:39:23 -0500

    Added subscript printer and molar ratios

--- equation.py
+++ equation.py
@@ -2,6 +2,21 @@ from chempy import chemistry
 from chempy.util import periodic 
 from chempy.util import parsing
 
+
+
+subscripts = {'0': '₀', '1': '₁', '2': '₂', '3': '₃', '4': '₄', '5': '₅', '6': '₆', '7': '₇', '8': '₈', '9': '₉'}
+
+def format_subscripts(string):
+    new_string = ""
+    for char in string:
+        if char.isdigit():
+            new_string += subscripts[char]
+        else:
+            new_string += char
+
+    return new_string
+
+
 #parse our text file for chemical equations
 #return an array of chemical equations
 def parse_equations():
@@ -32,11 +47,11 @@ def moles_to_grams(comp, moles):
 
 class ChemicalEquation:
     def __init__(self, reactants, prod):
+        self.formula = reactants + prod
         self.reactants = dict.fromkeys(prod)
         self.products = dict.fromkeys(reactants)
         
         self.bal_react, self.bal_prod = chemistry.balance_stoichiometry(self.reactants, self.products)
-
     def print(self):
         print("Equation: ")
         for react in self.reactants:
@@ -55,7 +70,55 @@ class ChemicalEquation:
 
         print("\n")
 
-    
+    def get_reactants(self):
+        return self.reactants
+    def get_products(self):
+        return self.products
+
+    def is_correct_coeff(self, coeff, compound):
+        print(self.bal_react)
+        print(self.bal_prod)
+        if compound in self.bal_react:
+            return self.bal_react[compound] == coeff
+        if compound in self.bal_prod:
+            return self.bal_prod[compound]  == coeff
+        return False
+
+    def as_string(self):
+        formula = ""
+        for react in self.reactants:
+            formula += react + " + "
+
+        formula = formula[:-3]
+        formula += " -> "
+        for prod in self.products:
+            formula += prod + " + "
+
+        formula = formula[:-3]
+        return format_subscripts(formula)
+
+
+                
+    def get_formula(self):
+        return self.formula.copy()
+
+    def get_molar_ratio(self, comp1, comp2):
+        coeff_one = 1
+        coeff_two = 1
+        if comp1 in self.bal_react:
+            coeff_one = self.bal_react[comp1]
+        elif comp1 in self.bal_prod:
+            coeff_one = self.bal_prod[comp1]
+
+        if comp2 in self.bal_react:
+            coeff_two  = self.bal_react[comp2]
+        elif comp2 in self.bal_prod:
+            coeff_two = self.bal_prod[comp2]
+
+        return coeff_one / coeff_two
+
+
+
 
 
 
