56311d055d1c25a876581ba403995d5750c0ad81
Brady-Brandt <bradybrandt3@gmail.com>
2023-05-13 15:38:43 -0500

    Added a new frame system, started implementing help section

--- main.py
+++ main.py
@@ -8,34 +8,65 @@ class Root:
         self.screen = screen
         self.WIDTH = WIDTH
         self.HEIGHT = HEIGHT
-        self.menu_widgets = []
         
-        self.practice_session = Practice(self.screen, self.WIDTH, self.HEIGHT)
-        self.GRAMS = "grams"
-        self.MOLES = "moles"
+
+        self.scrollbar = tk.Scrollbar(self.screen, orient='vertical')
+        self.menu_frame = tk.Frame(self.screen, bg='white')
+        self.practice_frame = tk.Frame(self.screen, bg='white')
+        self.help_frame = tk.Frame(self.screen, bg='white')
+        self.menu_button = tk.Button(self.screen, height = 2, text="Main Menu", command = self.show_main_menu)
+        #0 menu
+        # 1 practice
+        # 2 solver
+        self.which_frame = 0
     def show_main_menu(self):
+        if self.which_frame == 1:
+            for widget in self.practice_frame.winfo_children():
+                widget.destroy()
+            self.practice_frame.forget()
+        elif self.which_frame == 2:
+            for widget in self.help_frame.winfo_children():
+                widget.destroy()
+            self.help_frame.forget()
+
+         
+        self.menu_frame.pack()
         #main menu 
-        title = tk.Label(text = "Stoichiometry Interactive Guide")
-        title.config(font =('Helvetica bold', 25))
+        title = tk.Label(self.menu_frame,text = "Stoichiometry Interactive Guide", font=('Helvetica bold', 25), bg='white')
+        stoich_practice_button = tk.Button(self.menu_frame, bg='white', text="Stoichiometry Practice", width =20, height=3)
+        stoich_problem_button = tk.Button(self.menu_frame, bg='white', text="Stoichiometry Problem Help", width=20,height=3)
 
-        stoich_practice_button = tk.Button(screen, text="Stoichiometry Practice", width =20, height=3)
-        stoich_problem_button = tk.Button(screen, text="Stoichiometry Problem Help", width=20,height=3)
-
-        title.place(x = WIDTH / 2 - 200, y = 200)
-        stoich_practice_button.place(x=self.WIDTH / 2 - 100, y=self.HEIGHT / 2)
-        stoich_problem_button.place(x=self.WIDTH / 2 - 100, y=self.HEIGHT / 2 + 100)
+        title.pack(pady=20)
+        stoich_practice_button.pack(pady=10)
+        stoich_problem_button.pack(pady=10)
         
 
-        self.menu_widgets = [title, stoich_practice_button, stoich_problem_button]
+        stoich_problem_button.config(command=self.start_help)
         stoich_practice_button.config(command = self.start_practice) 
 
     def start_practice(self):
-        for widget in self.menu_widgets:
-            widget.place_forget()
-
-        self.practice_session.setup_problem()
+        for widget in self.menu_frame.winfo_children():
+            widget.destroy()
         
+        self.menu_frame.forget()
 
+        self.which_frame = 1
+        self.scrollbar.pack(side='right', fill='y', padx=2, pady=2)
+        self.menu_button.pack(side='top', anchor='nw')
+        practice_session = Practice(self.screen, self.WIDTH, self.HEIGHT, self.scrollbar, self.practice_frame)
+        practice_session.setup_problem()
+        
+    def start_help(self):
+        for widget in self.menu_frame.winfo_children():
+            widget.destroy()
+        self.menu_frame.forget()
+        self.which_frame = 2
+
+        self.scrollbar.pack(side='right', fill='y', padx=2, pady=2)
+        self.menu_button.pack(side='top', anchor='nw')
+        help_session = Practice(self.screen, self.WIDTH, self.HEIGHT, self.scrollbar, self.practice_frame)
+        help_session.setup_help()
+        
 
 
 
