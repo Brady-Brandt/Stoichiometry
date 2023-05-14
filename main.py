cab26b65958954dfac6cbbbd6e9e70a8cd67a880
Brady-Brandt <bradybrandt3@gmail.com>
2023-05-14 10:54:20 -0500

    Switched from frames to canvas and added scrollbar

--- main.py
+++ main.py
@@ -10,26 +10,24 @@ class Root:
         self.HEIGHT = HEIGHT
         
 
-        self.scrollbar = tk.Scrollbar(self.screen, orient='vertical')
         self.menu_frame = tk.Frame(self.screen, bg='white')
-        self.practice_frame = tk.Frame(self.screen, bg='white')
-        self.help_frame = tk.Frame(self.screen, bg='white')
+        self.canvas = tk.Canvas(self.screen, bg='white', highlightthickness=0, borderwidth=0)
         self.menu_button = tk.Button(self.screen, height = 2, text="Main Menu", command = self.show_main_menu)
+        self.scrollbar = tk.Scrollbar(self.screen, orient='vertical', command=self.canvas.yview)
+        self.scrollbar.pack()
         #0 menu
-        # 1 practice
-        # 2 solver
+        # 1 practice or help 
         self.which_frame = 0
-    def show_main_menu(self):
-        if self.which_frame == 1:
-            for widget in self.practice_frame.winfo_children():
-                widget.destroy()
-            self.practice_frame.forget()
-        elif self.which_frame == 2:
-            for widget in self.help_frame.winfo_children():
-                widget.destroy()
-            self.help_frame.forget()
 
-         
+        self.scrollbar.pack(side='right', fill='y')
+    def show_main_menu(self):
+
+        if self.which_frame == 1:
+            for widget in self.canvas.winfo_children():
+                widget.destroy()
+            self.canvas.forget()
+
+        self.menu_button.forget()         
         self.menu_frame.pack()
         #main menu 
         title = tk.Label(self.menu_frame,text = "Stoichiometry Interactive Guide", font=('Helvetica bold', 25), bg='white')
@@ -50,21 +48,20 @@ class Root:
         
         self.menu_frame.forget()
 
+
         self.which_frame = 1
-        self.scrollbar.pack(side='right', fill='y', padx=2, pady=2)
         self.menu_button.pack(side='top', anchor='nw')
-        practice_session = Practice(self.screen, self.WIDTH, self.HEIGHT, self.scrollbar, self.practice_frame)
+        practice_session = Practice(self.screen, self.WIDTH, self.HEIGHT, self.canvas, self.scrollbar)
         practice_session.setup_problem()
         
     def start_help(self):
         for widget in self.menu_frame.winfo_children():
             widget.destroy()
         self.menu_frame.forget()
-        self.which_frame = 2
+        self.which_frame = 1
 
-        self.scrollbar.pack(side='right', fill='y', padx=2, pady=2)
         self.menu_button.pack(side='top', anchor='nw')
-        help_session = Practice(self.screen, self.WIDTH, self.HEIGHT, self.scrollbar, self.practice_frame)
+        help_session = Practice(self.screen, self.WIDTH, self.HEIGHT, self.canvas, self.scrollbar)
         help_session.setup_help()
         
 
