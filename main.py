33ef84b17bec4701f638dc9cf6892e427cf5a62e
Brady-Brandt <bradybrandt3@gmail.com>
2023-07-23 09:42:24 -0500

    Set screen size restrictions

--- main.py
+++ main.py
@@ -13,10 +13,14 @@ class Root:
 
         self.menu_frame = tk.Frame(self.screen, bg='white')
         self.canvas = tk.Canvas(self.screen, bg='white', highlightthickness=0, borderwidth=0)
-        self.menu_button = tk.Button(self.screen, height = 2, text="Main Menu", command = self.show_main_menu)
+        self.menu_button = tk.Button(self.screen, height = 2,text="Main Menu", command = self.show_main_menu)
         self.scrollbar = tk.Scrollbar(self.screen, orient='vertical', command=self.canvas.yview)
         self.scrollbar.pack()
         self.periodic_table = PeriodicTable(self.screen)
+        # used to reference inside practice 
+        # not on the home screen 
+        self.periodic_button = tk.Button(self.screen, height = 2, text="Periodic Table", command = self.periodic_table.show_periodic_table)
+
         #0 menu
         # 1 practice or help 
         self.which_frame = 0
@@ -30,7 +34,9 @@ class Root:
             self.canvas.forget()
 
         self.menu_button.forget()         
+        self.periodic_button.forget()
         self.menu_frame.pack()
+
         #main menu 
         title = tk.Label(self.menu_frame,text = "Stoichiometry Interactive Guide", font=('Helvetica bold', 25), bg='white')
         stoich_practice_button = tk.Button(self.menu_frame, bg='white', text="Stoichiometry Practice", width =20, height=3)
@@ -57,8 +63,9 @@ class Root:
 
 
         self.which_frame = 1
-        self.menu_button.pack(side='top', anchor='nw')
-        practice_session = Practice(self.screen, self.WIDTH, self.HEIGHT, self.canvas, self.scrollbar)
+        self.menu_button.pack(side='left', anchor='nw')
+        self.periodic_button.pack(side='right', anchor='ne')
+        practice_session = Practice(self.screen, self.canvas, self.scrollbar)
         practice_session.setup_problem()
         
     def start_help(self):
@@ -67,15 +74,17 @@ class Root:
         self.menu_frame.forget()
         self.which_frame = 1
 
-        self.menu_button.pack(side='top', anchor='nw')
-        help_session = Practice(self.screen, self.WIDTH, self.HEIGHT, self.canvas, self.scrollbar)
+        self.menu_button.pack(side='left', anchor='nw')
+        self.periodic_button.pack(side='right', anchor='ne')
+        help_session = Practice(self.screen, self.canvas, self.scrollbar)
         help_session.setup_help()
 
-WIDTH = 800
-HEIGHT = 800
+WIDTH = 900
+HEIGHT = 600
 screen = tk.Tk()
-screen.geometry("800x800")
+screen.geometry("900x600")
 screen.title("Stoichiometry")
+screen.minsize(800, 500)
 screen["bg"] = "white"
 root = Root(screen, WIDTH, HEIGHT)
 root.show_main_menu()
